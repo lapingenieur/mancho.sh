a8929df8d505178ecca5485417ceca85ff962e1b
lapingenieur <ep.elio.poletti@gmail.com>
2020-07-29 11:27:27 +0200

    done help message

--- src/mancho.sh
+++ src/mancho.sh
@@ -41,7 +41,7 @@ version(){
 }
 
 help(){
-	cat << EOF
+	less << EOF
 	                                             /\$\$                             \$\$\\
 	                                            | \$\$                             \$\$ |
 	 /\$\$\$\$\$\$/\$\$\$\$   /\$\$\$\$\$\$  /\$\$\$\$\$\$\$   /\$\$\$\$\$\$\$| \$\$\$\$\$\$\$   /\$\$\$\$\$\$     \$\$\$\$\$\$\$\\ \$\$\$\$\$\$\$\\
@@ -55,13 +55,77 @@ help(){
 
 USAGE :
 
-	mancho.sh
-	mancho.sh COMMAND
-	mancho.sh OPTION
+	mancho.sh [--desc] [OPTION]
 	mancho.sh --mancho OPTION...
+	mancho.sh MAN_OPTIONS...
+	mancho.sh -- MAN_OPTIONS...
 
-following TODO
+DESCRIPTION :
+
+   Mancho.sh is a friendly interface for man. If you call it without any option (or just with "--desc"), mancho.sh will update its command
+   list (if needed) and pull up a fzf (fuzzy finder) research. You'll just need to enter the name or the description of the command you're
+   asking for and mancho.sh will open its manual page.
+
+   I recommend you to alias \`man\` to \`mancho.sh\` (add this line to ~/.bashrc if you are using bash : alias man='mancho.sh')
+
+   Mancho.sh has some builtin options (like the one you are using to print this help page). If mancho.sh gets an option it doesn't know, it
+   gives it to man, like \`mancho.sh --usage\` is gonna execute \`man --usage\`.
+
+      IMPORTANT :
+   
+   Note that if the first argument (or the one after --desc) is not recognized, mancho.sh will give EVERY arguments to man in the SAME com-
+   mand, whereas if it gets a known option for \$1, it will execute EVERY man options but only ONE a time. That's why there's a -- argument
+   which tells mancho.sh that all the following arguments are to be given to man. Ex:
+
+   mancho.sh --help getcpu 2
+     => this will print mancho.sh's help page, try to open getcpu's manual page (without the 2), and try to open 2's manual page
+
+   mancho.sh --help -- getcpu 2
+     => this will print mancho.sh's help page, and open getcpu (2)'s manual page.
+
+OPTIONS :
+
+   --sync
+      SYNCronization, syncronize the list files (more infos : part FILE)
+
+   --desc
+      DESCription, activate description mode : if no more arguments, will do a fzf search within the description of each command.
+      Not really useful unless you search for a description and not for a command name.
+
+   -h, --help (and -help)
+      Print this help page (mancho.sh's help page)
+
+   -q, --quick, --quick-help
+      Print a quick help (mancho.sh's quick help page)
+
+   -H, --man-help
+      Print man's help page (pass "-h" options to man)
+
+   --
+      Give all the following arguments to man
+
+FILES :
+
+   Mancho.sh uses two files which he creates they don't exist :
+   
+      ~/.config/mancho.sh/list
+         list of the installed manual pages (command names)
+
+      ~/.config/mancho.sh/list.desc
+         list of the installed manual pages (command names) followed by a short description
+
+   These two files are updated automaticaly every day or if mancho.sh receives "--sync".
+
+FAQ :
+
+   Q: Why are there files for the lists, can't just make the list at each run ?
+   A: These lists can be very long (several hundreds of lines) and it takes a bit of time to make them. It is more efficient to create a
+        daily list and output it into a file than wait before using mancho.sh everytimes.
 EOF
+}
+
+quickhelp(){
+	echo "TODO : quick-help"
 }
 
 ########## DEFINE YOUR STUFF HERE
@@ -90,14 +154,16 @@ fi
 if test "$*"		## arguments decoding
 then
 	case "$1" in			## if argument recognized :
-		"--sync" | "-h" | "--help" | "-help" | "-H" | "--man-help" | "--" )
+		"--sync" | "-h" | "--help" | "-help" | "-q" | "--quick" | "--quick-help" | "-H" | "--man-help" | "--" )
 			until test $# = 0 || test "$ok" = 1
 			do
 				case $1 in
 					"--sync" ) version ; echo "" ; sync ;;
 					"-h" | "--help" | "-help" ) help ;;
+					"-q" | "--quick" | "--quick-help" ) quickhelp ;;
 					"-H" | "--man-help" ) man -h ;;
 					"--" ) shift ; man $* ; ok=1 ;;
+					* ) man $1 ;;
 				esac
 				shift
 			done ;;		## if argument is not recognized, pass them all to man
