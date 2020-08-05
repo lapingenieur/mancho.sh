0c114d92cfbeca2c90940fd203614c69cabe7434
lapingenieur <ep.elio.poletti@gmail.com>
2020-08-05 18:00:04 +0200

    added src/config.sh and docs/help.md, help.md is output of mancho.sh -h

---
+++ src/config.sh
@@ -0,0 +1,13 @@
+# List of the internal variables you may want to change :
+# NAME :		DESCRIPTION :
+# verbose		verbosing option
+# desc			1 = use always description mode
+# fzf_height		fzf's height
+# fzf_options		fzf settings, merged into FZF_DEFAULT if set
+#
+# List of the environment variables you may want to change :
+#    Note : you need to define these variables using the 'export' command (see your shell's manual)
+# NAME :		DESCRIPTION :
+# FZF_DEFAULT_OPTS	fzf settings (prefer using the fzf_options internal variable)
+# MANPAGER		wanted program or script to print the manual page
+verbose=0
