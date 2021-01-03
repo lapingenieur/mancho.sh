8fb6768e52d7771feb2e38f8fc6bcbb2f80e37a2
lapingenieur <ep.elio.poletti@gmail.com>
2021-01-03 20:50:39 +0100

    [utils] continued working, now structured arg detection

--- src/mancho-utils.sh
+++ src/mancho-utils.sh
@@ -13,7 +13,7 @@
 # Do not change values here but in the config file (see help over the github page)
 
 vers=2.0	# current version
-verbose=1	# 1=verbosing mode (log things to stdout)
+verbose=0	# 1=verbosing mode (log things to stdout)
 author="lapingenieur"	# list of authors
 
 # The following variables are some kind of constants (which actually don't exist in bash)
@@ -53,7 +53,7 @@ vecho(){
 			1) echo -e "$2" ;;
 			2) echo "verb: $2" ;;
 			3) echo -e "verb: $2" ;;
-			*) log "utils.vecho/warn" "wrong arg 1 ('$1') => not verbosing" ;;
+			*) log "utils.vecho/err" "wrong arg 1 ('$1') => not verbosing" ;;
 		esac
 	fi
 }
@@ -61,18 +61,64 @@ vecho(){
 version(){
 # version - print mancho*.sh's version
 # usage : version DIST
-#   DIST : choose which slogan to print ( >> 1=mancho.sh ; 2=mancho-dmenu.sh ; 3=mancho-utils.sh << )
+#   DIST : choose which slogan to print ( >> 0=(nothing, print only version if no other) ; 1=mancho.sh ; 2=mancho-dmenu.sh ; 3=mancho-utils.sh << )
 #     `=> NOTE : prints if finds the character in $1 (31 = print mancho.sh and mancho-utils.sh ; 231 = print all ; 2 = print only mancho-dmenu.sh)
-	echo "$1" | grep "1" -q && echo -e "mancho.sh - $slogan"
-	echo "$1" | grep "2" -q && echo -e "mancho-dmenu.sh - $slogan (dmenu distribution)"
-	echo "$1" | grep "3" -q && echo -e "mancho-utils.sh - $utils_slogan"
+	version_done=0
+	echo "$1" | grep "1" -q && echo -e "mancho.sh - $slogan" && version_done=1
+	echo "$1" | grep "2" -q && echo -e "mancho-dmenu.sh - $slogan (dmenu distribution)" && version_done=1
+	echo "$1" | grep "3" -q && echo -e "mancho-utils.sh - $utils_slogan" && version_done=1
+	echo "$1" | grep "0" -q && version_done=1
+	if test $version_done = 0
+	then
+		log "utils.version/err" "wrong arg 1 : is void => printing only version"
+	fi
 	echo "version : v$vers"
 }
 
 utils_quickhelp(){
 	uqh_vers=2.0
 	version 3
-	vecho 0 ""
+	vecho 0 "utils_quickhelp version : $uqh_vers"
+	cat << EOF
+
+================================
+
+USAGE :
+   mancho-utils COMMAND [COMMAND_OPTION]...
+   mancho-utils OPTION...
+EOF
 }
 
-utils_quickhelp
+### mancho-utils.sh arguments detection/decoding ###
+
+if test $# -gt 0
+then
+##### first argument detection : global settings (like verbosing mode)
+	arg=0	# not shifting there, arg will be deleted
+	until test $arg -eq $#
+	do
+		arg=$((arg+1))
+		case "$(eval "echo \$$arg")" in
+			"--verbose" | "-V" ) verbose=1 ;;
+		esac
+	done
+	unset arg
+##### second argument detection : running actions
+	until test $# = 0
+	do
+		case "$1" in
+			"--verbose" | "-V" ) true ;;	# --verbose == true because it's detected above
+			"-version" | "--version" | "--vers" | "-v" ) version 3 ;;
+			"-help" | "--help" | "-h" ) utils_quickhelp ;;
+			"--exec" ) shift
+				$*
+				shift $#	# shifting until 0 to skip every other argument
+				;;
+			* ) echo "This argument is unknown : '$1'" ;;
+		esac
+		test $# != 0 && shift
+	done
+else
+	version 3
+	echo -e "\nmancho-utils.sh must receive at least one argument. Try out 'mancho-utils.sh --help'"
+fi
